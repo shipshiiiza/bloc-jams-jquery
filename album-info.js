843614972ac6058adcade393c8bf4a6f418949ba
shipshiiiza <56731082+shipshiiiza@users.noreply.github.com>
2020-03-09 12:08:00 -0700

    Delete album-info.js
    wrong place

--- album-info.js
+++
@@ -1,8 +0,0 @@
-{
-  $('#album-title').text(album.title);
-  $('img#album-cover-art').attr('src', album.albumArtUrl);
-  $('.artist').text(album.artist);
-  $('#release-info').text(album.releaseInfo);
-
-
-}
