2d600cba510fd43f1a4efbc47a83bdc239850e72
shipshiiiza <56731082+shipshiiiza@users.noreply.github.com>
2020-03-12 17:12:43 -0700

    Add files via upload
    fixed helper.js file for netlify

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,8 @@
 class Helper {
   playPauseAndUpdate(song) {
     player.playPause(song);
-    const duration = player.getDuration()
-    $('#time-control .total-time').text(player.prettyTime(duration));
+    const totalTime = player.currentlyPlaying.duration;
+    $('#time-control .total-time').text(player.prettyTime(totalTime));
 
   };
 
